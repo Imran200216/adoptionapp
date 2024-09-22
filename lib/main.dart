a7f0d79a0d57386aa46b54b32239d2081bcef4ff
imran <imranbabuji162002@gmail.com>
2024-09-22 23:37:53 +0530

    just added the provider.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:adoptionapp/constants/colors.dart';
+import 'package:adoptionapp/provider/get_started_provider.dart';
 import 'package:adoptionapp/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,15 +13,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: AppColors.primaryColor,
-        ),
-        useMaterial3: true,
-      ),
-      home: const SplashScreen(),
-    );
+    return MultiProvider(
+        providers: [
+          /// get started provider
+          ChangeNotifierProvider(
+            create: (_) => GetStartedProvider(),
+          ),
+        ],
+        builder: (context, child) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(
+                seedColor: AppColors.primaryColor,
+              ),
+              useMaterial3: true,
+            ),
+            home: const SplashScreen(),
+          );
+        });
   }
 }
---
+++ lib/provider/get_started_provider.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class GetStartedProvider extends ChangeNotifier{
+
+}
