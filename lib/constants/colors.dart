a60fef2499514ce2f7e4a68bcea40986d00f293d
imran <imranbabuji162002@gmail.com>
2024-10-03 23:37:21 +0530

    guest avatar screen has been finished.

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color primaryColor = const Color(0xFFF68971);
   static Color secondaryColor = Colors.white;
+  static Color primaryColor = const Color(0xFFF68971);
   static Color transparentColor = Colors.transparent;
   static Color titleColor = const Color(0xFFF68971);
   static Color subTitleColor = const Color(0xFF938B96);
--- lib/screens/avatar_screens/guest_avatar_screen.dart
+++ lib/screens/avatar_screens/guest_avatar_screen.dart
@@ -2,11 +2,10 @@ import 'package:adoptionapp/constants/colors.dart';
 import 'package:adoptionapp/provider/user_details_providers/guest_avatar_provider.dart';
 import 'package:adoptionapp/screens/nick_name_screens/guest_nick_name_screen.dart';
 import 'package:adoptionapp/widgets/custom_icon_btn.dart';
-
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:double_tap_to_exit/double_tap_to_exit.dart';
 import 'package:flutter/material.dart';
-
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
@@ -20,12 +19,13 @@ class GuestAvatarScreen extends StatefulWidget {
 }
 
 class _GuestAvatarScreenState extends State<GuestAvatarScreen> {
- void initState() {
+  @override
+  void initState() {
     super.initState();
     // Fetch avatars when the screen is initialized
     WidgetsBinding.instance.addPostFrameCallback((_) {
       final userGuestDetailsProvider =
-      Provider.of<GuestUserDetailsProvider>(context, listen: false);
+          Provider.of<GuestUserDetailsProvider>(context, listen: false);
       if (userGuestDetailsProvider.imageUrls.isEmpty) {
         userGuestDetailsProvider.fetchAvatars();
       }
@@ -36,7 +36,7 @@ class _GuestAvatarScreenState extends State<GuestAvatarScreen> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     final userGuestDetailsProvider =
-    Provider.of<GuestUserDetailsProvider>(context);
+        Provider.of<GuestUserDetailsProvider>(context);
 
     return DoubleTapToExit(
       snackBar: SnackBar(
@@ -53,33 +53,33 @@ class _GuestAvatarScreenState extends State<GuestAvatarScreen> {
       ),
       child: SafeArea(
         child: Scaffold(
-          backgroundColor: AppColors.primaryColor,
+          backgroundColor: AppColors.secondaryColor,
           bottomSheet: userGuestDetailsProvider.isAvatarUpdated
               ? Container(
-            padding:
-            const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.zero,
-              color: AppColors.primaryColor,
-            ),
-            child: CustomIconBtn(
-              btnHeight: size.height * 0.06,
-              btnWidth: size.width,
-              btnText: "All Set, Let's Go!",
-              btnBorderRadius: 4,
-              btnOnTap: () {
-                /// moving to the next screen
-                Navigator.pushReplacement(context,
-                    MaterialPageRoute(builder: (context) {
-                      return const GuestNickNameScreen();
-                    }));
-              },
-              btnIcon: Icons.navigate_next_outlined,
-              btnColor: AppColors.primaryColor,
-              btnTextColor: AppColors.secondaryColor,
-              btnIconColor: AppColors.secondaryColor,
-            ),
-          )
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.zero,
+                    color: AppColors.secondaryColor,
+                  ),
+                  child: CustomIconBtn(
+                    btnHeight: size.height * 0.06,
+                    btnWidth: size.width,
+                    btnText: "All Set, Let's Go!",
+                    btnBorderRadius: 4,
+                    btnOnTap: () {
+                      /// moving to the next screen
+                      Navigator.pushReplacement(context,
+                          MaterialPageRoute(builder: (context) {
+                        return const GuestNickNameScreen();
+                      }));
+                    },
+                    btnIcon: Icons.navigate_next_outlined,
+                    btnColor: AppColors.primaryColor,
+                    btnTextColor: AppColors.secondaryColor,
+                    btnIconColor: AppColors.secondaryColor,
+                  ),
+                )
               : const SizedBox(),
           body: Container(
             margin: const EdgeInsets.only(
@@ -90,35 +90,56 @@ class _GuestAvatarScreenState extends State<GuestAvatarScreen> {
             ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  "Add Cool Avatars\nfor your profile",
+                /// title
+                AutoSizeText(
+                  textAlign: TextAlign.start,
+                  'Set up your profile',
+                  maxLines: 2,
                   style: TextStyle(
-                    fontFamily: "Poppins",
+                    fontWeight: FontWeight.w800,
                     fontSize: size.width * 0.060,
-                    fontWeight: FontWeight.w700,
-                    color: AppColors.secondaryColor,
+                    color: AppColors.blackColor,
+                    fontFamily: "NunitoSans",
                   ),
                 ),
-                SizedBox(height: size.height * 0.05),
+                SizedBox(height: size.height * 0.01),
+
+                /// sub title
+                AutoSizeText(
+                  textAlign: TextAlign.start,
+                  'Add a cool avatars to make the profile more awesome and fun!',
+                  maxLines: 2,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: size.width * 0.040,
+                    color: AppColors.subTitleColor,
+                    fontFamily: "NunitoSans",
+                  ),
+                ),
+
+                SizedBox(height: size.height * 0.02),
                 Center(
                   child: DottedBorder(
-                    borderType: BorderType.Circle,
+                    borderType: BorderType.RRect,
                     dashPattern: const [6, 6],
-                    color: Colors.grey.shade200,
+                    color: Colors.grey.shade500,
+                    radius: const Radius.circular(12),
                     strokeWidth: 2,
                     child: Container(
-                      width: size.width * 0.42,
-                      height: size.width * 0.42,
+                      width: size.width,
+                      height: size.height * 0.40,
                       decoration: BoxDecoration(
-                        shape: BoxShape.circle,
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(14),
                         image: DecorationImage(
                           image: userGuestDetailsProvider.selectedAvatarURL !=
-                              null
+                                  null
                               ? CachedNetworkImageProvider(
-                              userGuestDetailsProvider.selectedAvatarURL!)
+                                  userGuestDetailsProvider.selectedAvatarURL!)
                               : const AssetImage(
-                              "assets/images/png/avatar-bg-img.png"),
+                                  "assets/images/png/avatar-bg-img.png"),
                           fit: BoxFit.cover,
                         ),
                       ),
@@ -128,64 +149,78 @@ class _GuestAvatarScreenState extends State<GuestAvatarScreen> {
                 SizedBox(height: size.height * 0.05),
                 userGuestDetailsProvider.imageUrls.isEmpty
                     ? Center(
-                  child: Column(
-                    children: [
-                      SizedBox(height: size.height * 0.02),
-                      Lottie.asset(
-                        'assets/images/animation/empty-animation.json',
-                        height: size.height * 0.3,
-                        fit: BoxFit.cover,
-                      ),
-                    ],
-                  ),
-                )
-                    : Expanded(
-                  child: GridView.builder(
-                    gridDelegate:
-                    const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      crossAxisSpacing: 12,
-                      mainAxisSpacing: 12,
-                      childAspectRatio: 1,
-                    ),
-                    itemCount:
-                    userGuestDetailsProvider.imageUrls.length,
-                    itemBuilder: (context, index) {
-                      final avatarUrl =
-                      userGuestDetailsProvider.imageUrls[index];
-
-                      return InkWell(
-                        onTap: () {
-                          userGuestDetailsProvider.setSelectedAvatar(
-                              avatarUrl, context);
-                        },
-                        child: Container(
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                          ),
-                          child: ClipOval(
-                            child: CachedNetworkImage(
-                              imageUrl: avatarUrl,
+                        child: Column(
+                          children: [
+                            SizedBox(height: size.height * 0.02),
+                            Lottie.asset(
+                              'assets/lottie/empty-animation.json',
+                              height: size.height * 0.3,
                               fit: BoxFit.cover,
-                              placeholder: (context, url) => Center(
-                                child:
-                                LoadingAnimationWidget.dotsTriangle(
-                                  color: AppColors.secondaryColor,
-                                  size: 40,
+                            ),
+                          ],
+                        ),
+                      )
+                    : Expanded(
+                        child: ListView.separated(
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          // Horizontal scrolling
+                          itemCount: userGuestDetailsProvider.imageUrls.length,
+                          itemBuilder: (context, index) {
+                            final avatarUrl =
+                                userGuestDetailsProvider.imageUrls[index];
+
+                            return Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 12.0),
+                              child: InkWell(
+                                onTap: () {
+                                  userGuestDetailsProvider.setSelectedAvatar(
+                                    avatarUrl,
+                                    context,
+                                  );
+                                },
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      width: size.width * 0.38,
+                                      height: size.height * 0.20,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(8),
+                                      ),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(8),
+                                        child: CachedNetworkImage(
+                                          imageUrl: avatarUrl,
+                                          fit: BoxFit.cover,
+                                          placeholder: (context, url) => Center(
+                                            child: LoadingAnimationWidget
+                                                .threeArchedCircle(
+                                              color: AppColors.primaryColor,
+                                              size: size.width * 0.20,
+                                            ),
+                                          ),
+                                          errorWidget: (context, url, error) =>
+                                              Icon(
+                                            Icons.error,
+                                            color: AppColors.primaryColor,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
-                              errorWidget: (context, url, error) =>
-                                  Icon(
-                                    Icons.error,
-                                    color: AppColors.secondaryColor,
-                                  ),
-                            ),
-                          ),
+                            );
+                          },
+                          separatorBuilder: (context, index) {
+                            return SizedBox(
+                              width: size.width * 0.04,
+                            ); // Adds space between the items
+                          },
                         ),
-                      );
-                    },
-                  ),
-                ),
+                      ),
+
                 SizedBox(height: size.height * 0.08),
               ],
             ),
